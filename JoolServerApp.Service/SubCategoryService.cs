86635160df58f46fc95ce85a2fc7b6d4319c3764
Benjamin Samuel Weiss <31832941+Biter29@users.noreply.github.com>
2021-09-09 14:43:38 -0400

    Fileschanged
    All files that don't have two primary key services

---
+++ JoolServerApp.Service/SubCategoryService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JoolServerApp.Repo;
+using JoolServerApp.Data;
+
+namespace JoolServerApp.Service
+{
+    public class SubCategoryService:ISubCategoryService
+    {
+        private IRepository<tblSubCategory> subCategoryRepository;
+        public SubCategoryService(IRepository<tblSubCategory> subCategoryRepository)
+        {
+            this.subCategoryRepository = subCategoryRepository;
+        }
+
+        public void DeleteSubCategory(long id)
+        {
+            tblSubCategory subCategory = subCategoryRepository.Get(id);
+            subCategoryRepository.Remove(subCategory);
+            subCategoryRepository.SaveChanges();
+        }
+
+        public IEnumerable<tblSubCategory> GetAllSubCategories()
+        {
+            return subCategoryRepository.GetAll();
+        }
+
+        public tblSubCategory GetSubCategory(long id)
+        {
+            return subCategoryRepository.Get(id);
+        }
+
+        public void insertSubCategory(tblSubCategory subCategory)
+        {
+            subCategoryRepository.Insert(subCategory);
+        }
+
+        public void UpdateSubCategory(tblSubCategory subCategory)
+        {
+            subCategoryRepository.Update(subCategory);
+        }
+    }
+}
