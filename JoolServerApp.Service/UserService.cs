2de04d436094fa4e83757302868dd4a08b9c50cf
Kelvin Paa Kwesi Adjei <kpkadjei1@gmail.com>
2021-09-08 22:09:08 -0400

    UserService interface implemented

---
+++ JoolServerApp.Service/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using JoolServerApp.Repo;
+using JoolServerApp.Data;
+
+namespace JoolServerApp.Service
+{
+    public class UserService:IUserService
+    {
+        private IRepository<tblUser> userRepository;
+
+        public UserService(IRepository<tblUser> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public IEnumerable<tblUser> GetUsers()
+        {
+            return userRepository.GetAll();
+        }
+        public tblUser GetUser(long id)
+        {
+            return userRepository.Get(id);
+        }
+        public void insertUser(tblUser user)
+        {
+            userRepository.Insert(user);
+        }
+        public void UpdateUser(tblUser user)
+        {
+            userRepository.Update(user)
+        }
+
+        public void DeleteUser(long id)
+        {
+            tblUser user = GetUser(id);
+            userRepository.Remove(user);
+            userRepository.SaveChanges();
+        }
+
+    }
+}
