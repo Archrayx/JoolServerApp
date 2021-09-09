b402f9c1a136f75a3d0b802ae3b9c97c4b142fc9
Kelvin Paa Kwesi Adjei <kpkadjei1@gmail.com>
2021-09-09 16:02:54 -0400

    Addded some changes. Error in home controller will be addressed later

--- JoolServerApp.Data/BaseEntity.cs
+++ JoolServerApp.Data/BaseEntity.cs
@@ -14,9 +14,9 @@ namespace JoolServerApp.Data
         }
 
         public Int64 Id { get; set; }
-            public DateTime AddedDate { get; set; }
-            public DateTime ModifiedDate { get; set; }
-            public string IPAddress { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string IPAddress { get; set; }
         
     }
 }
--- JoolServerApp.Web/Controllers/HomeController.cs
+++ JoolServerApp.Web/Controllers/HomeController.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JoolServerApp.Data;
+using JoolServerApp.Repo;
+using JoolServerApp.Service;
+using JoolServerApp.Web.Models;
+//using Microsoft.Extensions.DependencyInjection;
 
 namespace JoolServerApp.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController() { }
+        private readonly IUserService userService;
+
+
+        public HomeController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+
+        [HttpGet]
         public ActionResult Index()
         {
+            IEnumerable<tblUser> Users = userService.GetAllUsers();
+            List<UserViewModel> model = new List<UserViewModel>();
+            userService.GetAllUsers().ToList().ForEach(u =>
+            {
+                UserViewModel user = new UserViewModel
+                {
+                    Id = u.User_ID,
+                    FirstName = $"{u.User_Name}",
+
+                };
+                model.Add(user);
+            });
+
             return View();
         }
 
