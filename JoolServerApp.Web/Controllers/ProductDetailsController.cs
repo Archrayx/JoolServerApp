ba31fbad78d2e31947d3dcfb5d7672857a08538d
joseumeza <josemezadev@gmail.com>
2021-09-13 09:06:23 -0400

    comment

--- JoolServerApp.Web/Controllers/ProductDetailsController.cs
+++ JoolServerApp.Web/Controllers/ProductDetailsController.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using JoolServerApp.Service;
 using System.Web.Mvc;
+using JoolServerApp.Web.ViewModels;
 
 namespace JoolServerApp.Web.Controllers
 {
     public class ProductDetailsController : Controller
     {
-        // GET: ProductDetails
-        public ActionResult ProductDetails()
-        {
+        private readonly IProductService productService;
 
-            return View();
+        public ProductDetailsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        // GET: ProductDetails
+        public ActionResult ProductDetails(int id)
+        {
+            var item = this.productService.GetProduct(id);
+
+            ProductDetailsVM product = new ProductDetailsVM();
+            product.Product_ID = item.Product_ID;
+            product.Product_Name = item.Product_Name;
+            product.Series = item.Series;
+            product.Model = item.Model;
+            product.Manufacturer_ID = item.Manufacturer_ID;
+            product.Sales_ID = item.Sales_ID;
+            product.Series_Info = item.Series_Info;
+            product.SubCategory_ID = item.SubCategory_ID;
+
+
+            return View("ProductDetails", product);
         }
     }
 }
