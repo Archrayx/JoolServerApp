30b6e90ac71a32ffeb1bf56e2b21025043dc032e
joseumeza <josemezadev@gmail.com>
2021-09-13 01:49:36 -0400

    added controller and displayed information from db

--- JoolServerApp.Web/Controllers/ProductController.cs
+++ JoolServerApp.Web/Controllers/ProductController.cs
@@ -1,17 +1,31 @@
 ﻿using System;
+using JoolServerApp.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JoolServerApp.Web.ViewModels;
 
 namespace JoolServerApp.Web.Controllers
 {
     public class ProductController : Controller
     {
+        private JoolServerEntities db = new JoolServerEntities();
         // GET: Product
-        public ActionResult Index()
+        public ActionResult ProductSummary()
         {
-            return View("ProductSummary");
+
+            List<ProductVM> products = (from product in db.tblProducts
+                                        select new ProductVM
+                                        {
+                                            Product_ID = product.Product_ID,
+                                            Product_Name = product.Product_Name,
+                                            Series = product.Series,
+                                            Model = product.Model,
+                                            Series_Info = product.Series_Info,
+                                        }).ToList();
+
+            return View("ProductSummary", products);
         }
     }
 }
---
+++ JoolServerApp.Web/ViewModels/ProductVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JoolServerApp.Web.ViewModels
+{
+    public class ProductVM
+    {
+        public int? Product_ID { get; set; }
+        public string Product_Name { get; set; } 
+        //public string Product_Image { get; set; } 
+        public string Series { get; set; } 
+        public string Model { get; set; } 
+        public string Series_Info { get; set; }
+        
+    }
+}
