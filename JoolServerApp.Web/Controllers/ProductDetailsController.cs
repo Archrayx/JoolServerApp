cc62b1dee1c189960cf32fa36d9dd3340797605b
Benjamin Samuel Weiss <31832941+Biter29@users.noreply.github.com>
2021-09-13 04:39:27 -0400

    Commit
    Copy ProductDetailscontroller and ProductDetails.cshtml

---
+++ JoolServerApp.Web/Controllers/ProductDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoolServerApp.Web.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        // GET: ProductDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
