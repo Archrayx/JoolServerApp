3b83c4f8295049057cce89d2c71c82b36fdb7948
Benjamin Samuel Weiss <31832941+Biter29@users.noreply.github.com>
2021-09-19 15:07:19 -0400

    Project View
    Basic view model and part of view design, css must be worked on. Also controller as well. Which only has basic parts designed.

---
+++ JoolServerApp.Web/Controllers/ProjectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JoolServerApp.Service;
+using JoolServerApp.Repo;
+
+namespace JoolServerApp.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService projectService;
+        private readonly IUserService userService;
+        // GET: Project
+        public ProjectController(IProjectService projectService,IUserService userService)
+        {
+            this.projectService = projectService;
+            this.userService = userService;
+        }
+        public ActionResult Project()
+        {
+            
+            return View();
+        }
+
+ 
+    }
+}
