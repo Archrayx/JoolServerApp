ea860d3b3b9598e9ecea12bcea2d547394589e55
joseumeza <josemezadev@gmail.com>
2021-09-20 02:49:36 -0400

    testing

---
+++ JoolServerApp.Web/ViewModels/CompareVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JoolServerApp.Web.ViewModels
+{
+    public class CompareVM
+    {
+
+        public int? Product_ID { get; set; }
+        public string Product_Name { get; set; }
+    }
+}
